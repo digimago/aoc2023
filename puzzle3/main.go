5055732090cbc8a9107c7f7e0812c0424fc43ed0
Sebastiaan Visser <digimago@users.noreply.github.com>
2023-12-13 19:23:34 +0100

    merge conficts resolved

--- puzzle3/main.go
+++ puzzle3/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -23,9 +24,49 @@ func init() {
 
 }
 
+func isNumeric(r rune) bool {
+	if _, err := strconv.Atoi(string(r)); err != nil {
+		return false
+	}
+	return true
+}
+
+func isGear(x int, y int) bool {
+	// /var check bool
+	//check before
+	currentY := y - 1
+	if y > 0 && x > 0 {
+		fmt.Println(string(lines[currentY][x-1]))
+	}
+	//check after
+	if y > 0 && x > 0 {
+		fmt.Println(string(lines[currentY][x+1]))
+	}
+	//check line above
+	//check line below
+	return false
+}
+
 func main() {
 	pattern := regexp.MustCompile(`(?i)(?:)(\d+)`)
 
 	m := pattern.FindAllString(lines[0], -1)
 	fmt.Println(m)
+	fmt.Println(lines)
+
+	for i, j := range lines {
+		//processing a line
+		for k, l := range j {
+			if isNumeric(l) {
+				fmt.Println(i, k, string(l))
+				//check adjacency
+				fmt.Println(isGear(i, k))
+			}
+			if string(l) == "." {
+				continue
+			}
+			break
+
+		}
+	}
 }
